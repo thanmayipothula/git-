1a5f14a17bd73c22466fcb3168678a883e59a2cd
Pothula Thanmayi <thanmayi2308@gmail.com>
2024-08-14 12:50:36 +0530

    Added script.js file with basic JavaScript functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+function check(){
+let uname=document.getElementById("uname").value;
+let pswd=document.getElementById("pswd").value;
+document.getElementById("demo").innerHTML=uname;
+document.getElementById("dem").innerHTML=pswd;
+}
